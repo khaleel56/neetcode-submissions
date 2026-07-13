a627a6d1e553760669dad05c2ad960e5f9863e50
khaleel <94886195+khaleel56@users.noreply.github.com>
2026-07-13 23:44:34 +0530

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.js
@@ -0,0 +1,54 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        let m =matrix.length;
+        let n =matrix[0].length;
+        let left =0,right=m*n-1;
+        while(left<=right){
+            let mid = Math.floor((left+right)/2);
+            let row = Math.floor(mid/n)
+            let col = mid%n;
+            let val = matrix[row][col]
+
+            if(target == val){
+                return true
+            } else if(target<val){
+                right=mid-1
+            } else if(target>val){
+                left = mid+1
+            }      
+        }
+        // let i=0, j=matrix.length-1
+        // while(i<=j){
+        //     let mid1=math.floor((i+j)/2)
+        //     let array=matrix[i]
+        //     if(array[0]<= target && target<=array[array.length-1]){
+        //        let left = 0, right=array.length-1
+        //         while(left<=right){
+        //             let mid= Math.floor((left+right)/2)
+        //             if(target=== array[mid]){
+        //                 return true
+        //             } else if(target<array[mid]){
+        //                   right = mid-1
+        //             } else if(target>array[mid]){
+        //                 left =mid+1
+        //             }
+        //         }
+        //         return false
+
+        //     } else i++
+        // }
+        // for(let i=0;i<matrix.length;i++){
+        //     for(let j=0;j<matrix[i].length;j++){
+        //         if(matrix[i][j]=== target){
+        //             return true
+        //         }
+        //     }
+        // }
+        return false
+    }
+}
